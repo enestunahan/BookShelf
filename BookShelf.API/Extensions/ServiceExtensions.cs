92c59cadff99ff81421cb6e3edb51e3179d6503f
Enes Tunahan Çömez <enesstunahan@gmail.com>
2022-10-31 00:24:09 +0300

    AppDbContext were created and db connection established

---
+++ BookShelf.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BookShelf.Repository.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelf.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("MsSqlConnnection"));
+            });
+        }
+    }
+}
---
+++ BookShelf.Repository/DbContexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BookShelf.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelf.Repository.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<BookDetail> BookDetails { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
